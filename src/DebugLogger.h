11aa67f7a0e2be8a090e10d3bc5328aa2115cf80
Max Rigout <71095568+maxrigout@users.noreply.github.com>
2023-02-16 18:35:36 -0500

    added support for Windows (using MSBuild)

--- src/DebugLogger.h
+++ src/DebugLogger.h
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
 
-#define LOG_DEBUG(msg) do {			\
-	std::cout << "[" << "" << "] - '" << __func__ << "'::" << "'" << __FILE__ << "'::" << __LINE__ << "\n\t" << msg << "\n=====" << std::endl; \
+#define LOG_DEBUG(msg) do { \
+	std::cout << "[" << "" << "] - '" << __FILE__ << "'::" << "'" << __func__ << "'::" << __LINE__ << "\n\t" << msg << "\n=====" << std::endl; \
 } while(false)
