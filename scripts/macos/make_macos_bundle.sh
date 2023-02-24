b279c9c3f8fd2e2253f65bdb3df3f00abe9f365a
Max Rigout <71095568+maxrigout@users.noreply.github.com>
2023-02-23 23:26:13 -0500

    fixed ci

--- scripts/macos/make_macos_bundle.sh
+++ scripts/macos/make_macos_bundle.sh
@@ -5,6 +5,9 @@ PROJECT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd ..
 
 cd $PROJECT_DIR
 mkdir -p $OUTPUT_BUNDLE_NAME.app/Contents/MacOS
-mkdir -p $OUTPUT_BUNDLE_NAME.app/Contents/Resources
+mkdir -p $OUTPUT_BUNDLE_NAME.app/Contents/Resources/graphics
+mkdir -p $OUTPUT_BUNDLE_NAME.app/Contents/Resources/fonts
+touch $OUTPUT_BUNDLE_NAME.app/Contents/Resources/fonts/font
+touch $OUTPUT_BUNDLE_NAME.app/Contents/Resources/graphics/board
 cp $INPUT_BIN $OUTPUT_BUNDLE_NAME.app/Contents/MacOS/$OUTPUT_BUNDLE_NAME
 cp -R resources/ $OUTPUT_BUNDLE_NAME.app/Contents/Resources
