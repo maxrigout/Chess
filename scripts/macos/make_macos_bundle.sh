b8975dd4e2d62eee1911b531579d0289f4e3a5f4
Max Rigout <71095568+maxrigout@users.noreply.github.com>
2023-02-25 13:17:20 -0500

    fixing issues with .app

--- scripts/macos/make_macos_bundle.sh
+++ scripts/macos/make_macos_bundle.sh
@@ -7,6 +7,7 @@ cd $PROJECT_DIR
 mkdir -p $OUTPUT_BUNDLE_NAME.app/Contents/MacOS
 mkdir -p $OUTPUT_BUNDLE_NAME.app/Contents/Resources
 cp $INPUT_BIN $OUTPUT_BUNDLE_NAME.app/Contents/MacOS/$OUTPUT_BUNDLE_NAME
+chmod +x $OUTPUT_BUNDLE_NAME.app/Contents/MacOS/$OUTPUT_BUNDLE_NAME
 cp -R resources/ $OUTPUT_BUNDLE_NAME.app/Contents/
 cp scripts/macos/rsc/Info.plist chess.app/Contents/Info.plist
 cp scripts/macos/rsc/chess.icns chess.app/Contents/Resources/chess.icns
