d1c6ab2e73e658c117797c4858955e933d2373af
Max Rigout <71095568+maxrigout@users.noreply.github.com>
2023-03-12 21:05:37 -0400

    added missing config file

---
+++ src/Core/PlatformConfig.h
@@ -0,0 +1,19 @@
+#if defined _WIN32
+#define SUPPORT_OPENGL
+#define SUPPORT_VULKAN
+#define SUPPORT_DIRECTX
+#define SUPPORT_SDL
+#define SUPPORT_GLFW
+#define SUPPORT_WIN32
+#elif defined __APPLE__
+#define SUPPORT_OPENGL
+#define SUPPORT_VULKAN
+#define SUPPORT_METAL
+#define SUPPORT_SDL
+#define SUPPORT_GLFW
+#elif defined __linux__
+#define SUPPORT_OPENGL
+#define SUPPORT_VULKAN
+#define SUPPORT_SDL
+#define SUPPORT_GLFW
+#endif
